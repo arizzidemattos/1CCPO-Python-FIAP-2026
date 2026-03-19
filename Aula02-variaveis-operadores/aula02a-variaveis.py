3ae213a824581cc7b338216ad8bdbd967522de9f
arizzidemattos <arizzidemattos@gmail.com>
2026-03-19 20:57:10 -0300

    aula 2 variaveis e operadores

---
+++ Aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,24 @@
+print("ola mundo")
+
+print(7+4)
+print("7+4")
+print("7"+"4") #concatenaçao de strings (#= comentario)
+
+
+
+
+#variaveis
+nome = "Alexandre"
+print (nome)
+idade = 19
+peso = 79
+print(nome, idade, peso)
+print(f"oiiiii, {nome}!!!!!!")
+#INPUT - -SIMULAÇAO DE FORMS NO CMD
+nome = input("Digite o seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+ print(nome, idade, peso)
+print(idade + 1)
+ano_nascimento = 2007
+ano_atual = 2026
---
+++ Aula02-variaveis-operadores/desafio 1.py
@@ -0,0 +1,13 @@
+nome = input("Qual seu nome?")
+print(f"Boas Vindas, {nome}!")
+#Desafio 2
+dia_do_nascimento = input("Digite o dia do seu nascimento")
+mes_de_nascimento = input("Digite o mes do seu nascimento")
+ano_do_nascimento = input("Digite o ano de seu nascimento")
+print(f"Sua data de nascimento é: {dia_do_nascimento}/{mes_de_nascimento}/{ano_do_nascimento}")
+
+#Desafio 3
+num_1 = int(input("Digite um numero: "))
+num_2 = int(input("Digite outro numero:  "))
+print("O valor da soma é: ")
+print(f"{num_1+num_2}")
