65e9d630a21667fa387c8a64222f1c8dab633c7c
arizzidemattos <arizzidemattos@gmail.com>
2026-04-30 22:38:51 -0300

    aula05

---
+++ aula05-strings-array/atividade1-Duplas.py
@@ -0,0 +1,8 @@
+nomes = ["joao", "lucas", "jair", "luis"]
+for i in range(len(nomes)):
+
+    for j in range(i+1,len(nomes)):
+        print(nomes[0], nomes[j])
+
+
+
---
+++ aula05-strings-array/aula05b-arrays.py
@@ -0,0 +1,28 @@
+lista_frutas = ["laranja","uva", "melao"]
+
+
+lista_frutas[0] = "laranja"
+lista_frutas[1] = "uva"
+lista_frutas[2] = "melao"
+
+print(lista_frutas[1])
+lista_frutas.append("maca")
+print(lista_frutas)
+#lista_frutas[3] = "maca"
+
+for i in range(len(lista_frutas)):
+    print(lista_frutas[i])
+
+print()
+
+
+for fruta in lista_frutas:
+    print(fruta)
+
+
+
+
+
+
+
+
